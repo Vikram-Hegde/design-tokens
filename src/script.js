e18c94313515673f8fa202591b370756a8b2bbfe
Vikram-Hegde <vikramhegde1440@gmail.com>
2022-08-29 10:37:00 +0530

    misc colors can be added

---
+++ src/script.js
@@ -0,0 +1,11 @@
+//@ts-check
+
+/** @type {HTMLInputElement | null} */
+let range = document.querySelector('.range');
+
+/** @type {HTMLHtmlElement | null} */
+let html = document.querySelector('html');
+
+range?.addEventListener('input', () => {
+	html?.style.setProperty('--h', `${range?.value}deg`);
+});
